c69e8379273a791acf0f5580a138e95a595b1b73
EC2 Default User <ec2-user@ip-172-31-9-2.ec2.internal>
2022-07-04 00:35:02 +0000

    third commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'lists/start'
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'lists/destroy'
+  get 'books/top'
+  get 'homes/top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
