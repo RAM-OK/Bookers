8d5a819efeede751f8552c0b57c4924390853d1f
EC2 Default User <ec2-user@ip-172-31-9-2.ec2.internal>
2022-07-09 11:10:47 +0000

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,9 @@
 class BooksController < ApplicationController
   def top
   end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/start'
-  get 'lists/new'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'lists/destroy'
-  get 'books' => 'books#index'
+  resources :books
+  
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
