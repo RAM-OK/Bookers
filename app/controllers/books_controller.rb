a51ca37a9a1acc3aaadd5a199226027d8dde4867
EC2 Default User <ec2-user@ip-172-31-9-2.ec2.internal>
2022-07-11 15:25:01 +0000

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,15 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+  
+  def create
+   book = Book.new(book_params)
+   book.save
+   redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
